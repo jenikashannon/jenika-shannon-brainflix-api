b978582b606d0674ad9bfb150538f496b06e8b8b
Jenika Shannon <jenikashannon@pm.me>
2024-03-04 14:56:34 -0500

    add /videos endpoint

--- routes/videos.js
+++ routes/videos.js
@@ -3,11 +3,31 @@ const router = express.Router();
 const fs = require("fs");
 
 const videos = JSON.parse(fs.readFileSync("./data/videos.json"));
-console.log(videos);
+console.log(videos[0]);
+
+router.get("/", (req, res) => {
+	// get relevant details for each video
+	const videoList = videos.map((video) => {
+		const videoListItem = {
+			id: video.id,
+			title: video.title,
+			image: video.image,
+			channel: video.channel,
+		};
+
+		return videoListItem;
+	});
+
+	res.send(videoList);
+});
 
 router.get("/:videoId", (req, res) => {
 	const videoId = req.params.videoId;
-	res.send(videos.find((video) => video.id === videoId));
+
+	// find the video with the required video id
+	const video = videos.find((video) => video.id === videoId);
+
+	res.send(video);
 });
 
 module.exports = router;
