05a318837916f1784092caf377f53ee3c5a4dc40
Jenika Shannon <jenikashannon@pm.me>
2024-03-07 09:36:16 -0500

    set up add comment endpoint; created getVideoId function to set video id to first video in array, if none is provided

--- routes/videos.js
+++ routes/videos.js
@@ -5,8 +5,6 @@ const uniqid = require("uniqid");
 
 const pathToData = "./data/videos.json";
 
-// console.log(videos[0]);
-
 router
 	.route("/")
 	.get((req, res) => {
@@ -40,7 +38,7 @@ router
 
 router.get("/:videoId", (req, res) => {
 	const videos = readDatabase();
-	const videoId = req.params.videoId;
+	const videoId = getVideoId(req.params.videoId);
 
 	// find the video with the required video id
 	const video = videos.find((video) => video.id === videoId);
@@ -48,21 +46,41 @@ router.get("/:videoId", (req, res) => {
 	res.send(video);
 });
 
-router.delete("/:videoId/comments/:commentId", (req, res) => {
+router.post("/:videoId/comments", (req, res) => {
 	const videos = readDatabase();
-	const videoId = req.params.videoId;
-	const commentId = req.params.commentId;
+	const videoId = getVideoId(req.params.videoId);
+
+	const newComment = {
+		...req.body,
+		id: uniqid(),
+		timestamp: new Date().getTime(),
+	};
 
 	// find relevant video
 	const videoIndex = videos.findIndex((video) => video.id === videoId);
 
 	const video = videos[videoIndex];
 
+	video.comments.push(newComment);
+
+	writeToDatabase(videos);
+
+	res.send(newComment);
+});
+
+router.delete("/:videoId/comments/:commentId", (req, res) => {
+	const videos = readDatabase();
+	const videoId = getVideoId(req.params.videoId);
+	const commentId = req.params.commentId;
+
+	// find relevant video
+	const videoIndex = videos.findIndex((video) => video.id === videoId);
+	const video = videos[videoIndex];
+
 	// find relevant comment
 	const commentIndex = video.comments.findIndex(
 		(comment) => comment.id === commentId
 	);
-
 	const comment = video.comments[commentIndex];
 
 	// remove comment
@@ -90,4 +108,11 @@ function readDatabase() {
 
 function writeToDatabase(newData) {
 	fs.writeFileSync(pathToData, JSON.stringify(newData));
+}
+
+function getVideoId(videoId) {
+	const firstVideoId = readDatabase()[0].id;
+
+	// if no video id, set to first video in video list
+	return (videoId === "undefined" ? undefined : videoId) || firstVideoId;
 }
